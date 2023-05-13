a0fae246156c29768ffd1d3322e8f5b26abbe0a4
Arsene <chauanh1402@gmail.com>
2023-05-13 10:07:35 +0700

    Delete Game

--- src/Components/EditGame/EditGame.js
+++ src/Components/EditGame/EditGame.js
@@ -1,6 +1,8 @@
 import styles from './EditGame.module.css';
 import React from 'react';
 import AnimatedCardNoInit from '../../Containers/AnimatedPage/AnimatedCardNoInit';
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {makeRequest} from "../../axios";
 
 const EditGame = props => {
     const {
@@ -8,8 +10,29 @@ const EditGame = props => {
         handleHoverGame
     } = props;
 
+    const queryClient = useQueryClient();
+
+    const deleteMutation = useMutation(
+        (gameId) => {
+            return makeRequest.delete("game/delete", { data: gameId });
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(["games"]);
+            },
+        }
+    );
+
+
+    const handleDelete = () => {
+        let idNumber;
+        idNumber = parseInt(game.id, 10);
+        deleteMutation.mutate({ id: idNumber });
+    };
+
     return (
-        <div className={styles.addToCart} onMouseEnter={handleHoverGame} onMouseLeave={handleHoverGame} id={game.id}>
+        <div className={styles.addToCart} onMouseEnter={handleHoverGame} onMouseLeave={handleHoverGame} onClick={handleDelete} id={game.id}>
             <h4 style={{ color: game.isHovered ? "#92f" : "#999" }}>Delete</h4>
         </div>
     );
